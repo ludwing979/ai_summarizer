c34e4bcc14f7fbd2825eb7e909c347d8fd90a910
ludwing979 <magoludwing@gmail.com>
2023-05-12 13:11:39 -0600

    Adding last part of functionality

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -10,6 +10,7 @@ const Demo = () => {
   })
 
   const [allArticles, setAllArticles] = useState([])
+  const [copied, setCopied] = useState("")
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery()
   
@@ -39,6 +40,12 @@ const Demo = () => {
     }
   }
 
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl)
+    navigator.clipboard.writeText(copyUrl)
+    setTimeout(() => setCopied(false), 3000);
+  }
+
   return (
     <section className='mt-16 w-full max-w-xl'>
       {/* Search */}
@@ -54,8 +61,8 @@ const Demo = () => {
         <div className='flex flex-col gap-1 max-h-60 overflow-y-auto'>
           {allArticles.map((item, index) => (
             <div key={`link-${index}`} onClick={() => setArticle(item)} className='link_card'>
-              <div className='copy_btn'>
-                <img src={copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain' />
+              <div className='copy_btn' onClick={() => handleCopy(item.url)}>
+                <img src={copied === item.url ? tick : copy} alt="copy_icon" className='w-[40%] h-[40%] object-contain' />
               </div>
               <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>{item.url}</p>
             </div>
